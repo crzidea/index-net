46fbe727171dc83eb9e9ed81a59cad8c80946046
Crzidea <micro-tech@foxmail.com>
2016-02-11 10:32:01 +0000

    Change APIs

--- index.js
+++ index.js
@@ -5,58 +5,72 @@ var qs = require('querystring')
 var urls = {}
 urls.root = 'https://api.wmcloud.com/data/v1'
 //urls.api = '/api/market/getMktIdxdJY.json'
-urls.api = '/api/idx/getMktEquIdxdCCXE.json'
+//urls.api = '/api/idx/getMktEquIdxdCCXE.json'
 //urls.api = '/api/market/getMktMonthJY.json'
+urls.apiHistory = '/api/market/getMktIdxd.json'
+urls.apiLatest = '/api/market/getTickRTSnapshot.json'
 
-var query = {
-  //secID: '000001.XSHG',
+var queryHistory = {
   indexID: '000001.ZICN',
   //startDate: '19700101',
   //endDate: '20160101'
+  ticker: '000001'
 }
-var search = qs.stringify(query)
+var searchHistory = qs.stringify(queryHistory)
+
+var queryLatest = {
+  securityID: '000001.XSHG'
+}
+var searchLatest = qs.stringify(queryLatest)
 
 var headers = { Authorization: `Bearer ${process.env.WMCLOUD_TOKEN}` }
 
-exports.pathSave = `${process.env.HOME}/.index-net-${query.indexID}.json`
+exports.pathSave = `${process.env.HOME}/.index-net-${queryHistory.indexID}.json`
 
 function fetchData() {
-  return fetch(`${urls.root}${urls.api}?${search}`, { headers })
-  .then((res) => {
-    return res.json()
-  })
-  .then((body) => {
-    var data = body.data.reverse()
-    return normalize(data)
+  return Promise.all([
+    fetchAPI(urls.apiHistory, searchHistory),
+    fetchAPI(urls.apiLatest, searchLatest),
+  ])
+  .then((bodies) => {
+    //var data = body.data.reverse()
+    return normalize(bodies[0].data, bodies[1].data[0])
   })
   .catch((error) => {
     console.error(error.stack);
   })
 }
 
+function fetchAPI(api, search) {
+  return fetch(`${urls.root}${api}?${search}`, { headers })
+  .then((res) => res.json())
+}
+
 var day = 24 * 3600 * 1000
 
-function normalize(data) {
+function normalize(history, latest) {
   var past = []
-  for (var i = 1, l = data.length - 2; i < l; i++) {
-    var date = new Date(data[i].tradeDate);
+  for (var i = 1, l = history.length - 2; i < l; i++) {
+    var date = new Date(history[i + 1].tradeDate)
     var input = {
       date: date.valueOf(),
-      preClosePrice: data[i].preClosePrice,
-      openIndex: data[i].openIndex,
-      highestIndex: data[i - 1].highestIndex,
-      lowestIndex: data[i - 1].lowestIndex,
-      turnoverValue: data[i - 1].turnoverValue,
-      turnoverVol: data[i - 1].turnoverVol
+      openIndex: history[i].openIndex,
+      closeIndex: history[i].closeIndex,
+      preOpenIndex: history[i - 1].openIndex,
+      preCloseIndex: history[i - 1].closeIndex,
+      preHighestIndex: history[i - 1].highestIndex,
+      preLowestIndex: history[i - 1].lowestIndex,
+      preTurnoverValue: history[i - 1].turnoverValue,
+      preTurnoverVol: history[i - 1].turnoverVol
     }
-    //var output = {tomorrowCloseIndex: data[i + 1].closeindex}
-    var change = data[i + 1].closeindex - data[i].openIndex
+    //var output = {tomorrowCloseIndex: history[i + 1].closeindex}
+    var change = history[i + 1].closeIndex - history[i].openIndex
     //var output = {change}
-    //var percent = Math.round(change / data[i].openIndex * 100)
+    //var percent = Math.round(change / history[i].openIndex * 100)
     //var output = {percent}
-    var radio = change / data[i].openIndex
-    var abs = Math.abs(radio)
-    var output = { increase: radio > 0 ? 1 : 0 }
+    var ratio = change / history[i].openIndex
+    var abs = Math.abs(ratio)
+    var output = { increase: ratio > 0 ? 1 : 0 }
     for (var j = 1, stalls = 10; j <= stalls; j++) {
       var rangeA = (j - 1) / 100
       var rangeB = j / 100
@@ -66,20 +80,25 @@ function normalize(data) {
   }
 
   var future = []
-  var input = {}
-  var latestData = data[data.length - 1]
-  var latestDate = new Date(latestData.tradeDate)
+  var latestDate = new Date(latest.dataDate)
+  var date
   if (5 === latestDate.getDay()) {
-    input.date = latestDate.valueOf() + day * 2 + day
+    date = latestDate.valueOf() + day * 2 + day
   } else {
-    input.date = latestDate.valueOf() + day
+    date = latestDate.valueOf() + day
   }
-  input.preClosePrice = latestData.preClosePrice
-  input.openIndex = latestData.openIndex
-  input.highestIndex = data[data.length - 2].highestIndex
-  input.lowestIndex = data[data.length - 2].lowestIndex
-  input.turnoverValue = data[data.length - 2].turnoverValue
-  input.turnoverVol = data[data.length - 2].turnoverVol
+  var lastHistory = history[history.length - 1]
+  var input = {
+    date: date.valueOf(),
+    openIndex: latest.openPrice,
+    closeIndex: latest.lastPrice,
+    preOpenIndex: lastHistory.openIndex,
+    preCloseIndex: lastHistory.closeIndex,
+    preHighestIndex: lastHistory.highestIndex,
+    preLowestIndex: lastHistory.lowestIndex,
+    preTurnoverValue: lastHistory.turnoverValue,
+    preTurnoverVol: lastHistory.turnoverVol
+  }
   future.push({input})
 
   return {past, future}
